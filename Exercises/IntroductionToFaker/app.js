e3c70e2ff25c2fef7768059a3e08eda9cabc17e6
Edgar Dewsbery <edewsbery@gmail.com>
2019-08-15 10:01:20 +0100

    create basic query

--- Exercises/IntroductionToFaker/app.js
+++ Exercises/IntroductionToFaker/app.js
@@ -1,4 +1,21 @@
 var faker = require('faker');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  // TODO: create user that can interact with db
+  database: 'join_us'
+})
+
+connection.query('SELECT 1 + 1 AS SOLUTION', function (error, results, fields) {
+  if (error) {
+    console.log('DB Error:', error);
+  }
+
+  console.log('result: ', results)
+})
+
+connection.end();
 
 function generateAddress() {
   console.log(faker.address.streetAddress());
