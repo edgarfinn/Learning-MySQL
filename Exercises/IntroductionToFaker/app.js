474665cb83f37d5372cd065097fd8aa1b94c2e57
Edgar Dewsbery <edewsbery@gmail.com>
2019-08-19 09:09:37 +0100

    exercises

--- Exercises/IntroductionToFaker/app.js
+++ Exercises/IntroductionToFaker/app.js
@@ -7,15 +7,41 @@ var connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: 'join_us'
 })
+
+// SELECTING DATA ==========
+// connection.connect();
+// connection.query('SELECT COUNT(*) AS total FROM users', function(error, results, fields) {
+//    if (error) throw error;
+//    console.log('Results: ', results[0].total);
+// });
+// connection.end();
+
+// ADDING DATA DYNAMICALLY ==========
+// var person = {
+//   email: faker.internet.email(),
+//   created_at: faker.date.past()
+// };
+// connection.connect();
+// connection.query('INSERT INTO users SET ?', person, function(error, results, fields) {
+//   if (error) throw error;
+//   console.log('Results:', results);
+// })
+// connection.end();
+
+
+// INSERTING LOTS OF DATA ==========
+var data = [];
+for (var i = 0; i < 500; i++) {
+  data.push([
+    faker.internet.email(),
+    faker.date.past()
+  ])
+};
+var q = 'INSERT INTO users (email, created_at) VALUES ?';
+
 connection.connect();
-connection.query('SELECT 1 + 1 AS solution', function(error, results, fields) {
-   if (error) throw error;
-   console.log('The solution is: ', results[0].solution);
+connection.query(q, [data], function(error, result, fields) {
+  if (error) throw error;
+  console.log('Result', result);
 });
 connection.end();
-
-function generateAddress() {
-  console.log(faker.address.streetAddress());
-  console.log(faker.address.city());
-  console.log(faker.address.state());
-}
